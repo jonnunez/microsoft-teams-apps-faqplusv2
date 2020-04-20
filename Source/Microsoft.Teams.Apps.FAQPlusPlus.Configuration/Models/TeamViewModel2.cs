9297379e1603cb37b5012a5c16a59dceddf77303
Jonathan Nunez <Jonathan.Nunez@popular.com>
2020-04-20 10:29:11 -0400

    added duplicate functions but for 2nd field

---
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus.Configuration/Models/TeamViewModel2.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="TeamViewModel.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.FAQPlusPlus.Configuration.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Represents TeamViewModel object to store team details.
+    /// </summary>
+    public class TeamViewModel2
+    {
+        /// <summary>
+        /// Gets or sets team id textbox to be used in View.
+        /// </summary>
+        [Required(ErrorMessage = "Enter team id 2.")]
+        [MinLength(1)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Team ID 2")]
+        [RegularExpression(@"(\S)+", ErrorMessage = "Enter team id 2 which should not contain any whitespace.")]
+        public string TeamId2 { get; set; }
+    }
+}
