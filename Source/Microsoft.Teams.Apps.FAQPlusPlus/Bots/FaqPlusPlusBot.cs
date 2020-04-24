209673630c57411c7bac94a324244a3efbd2575f
Jonathan Nunez <Jonathan.Nunez@popular.com>
2020-04-24 19:58:49 -0400

    .

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Bots/FaqPlusPlusBot.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Bots/FaqPlusPlusBot.cs
@@ -387,8 +387,8 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Bots
                 string expertRequestId = await this.configurationProvider.GetSavedEntityDetailAsync(ConfigurationEntityTypes.TeamId).ConfigureAwait(false);
                 string expertfeedbackId = await this.configurationProvider.GetSavedEntityDetailAsync(ConfigurationEntityTypes.ChannelId).ConfigureAwait(false);
 
-                // teamsChannelData?.Team?.Id == expertRequestId
-                if (turnContext != null && teamsChannelData?.Channel?.Id == expertfeedbackId && await this.IsMemberOfSmeTeamAsync(turnContext).ConfigureAwait(false))
+                // teamsChannelData?.Channel?.Id == expertfeedbackId
+                if (turnContext != null && teamsChannelData?.Team?.Id == expertRequestId && await this.IsMemberOfSmeTeamAsync(turnContext).ConfigureAwait(false))
                 {
                     var messageExtensionQuery = JsonConvert.DeserializeObject<MessagingExtensionQuery>(turnContextActivity.Value.ToString());
                     var searchQuery = this.GetSearchQueryString(messageExtensionQuery);
